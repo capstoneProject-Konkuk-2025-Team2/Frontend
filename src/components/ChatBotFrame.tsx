91979aa7e5e467dfb09f2615a3dc45028fba277e
glyph8 <newdy0808@gmail.com>
2025-05-27 23:30:08 +0900

    feat: 챗봇 프레임 생성

--- src/components/ChatBotFrame.tsx
+++ src/components/ChatBotFrame.tsx
@@ -1,19 +1,48 @@
 import { useChatBotPageStore } from "../store/store"
 
-const ChatBotFrame = () =>{
+const ChatBotFrame = () => {
 
-    const {isChatBotOpen} = useChatBotPageStore();
+    const { openChatBotPage } = useChatBotPageStore();
 
-    return(
-        <>
-        {
-            isChatBotOpen ? (
-                <div>
-                    445
+    return (
+        <div>
+            {/* <div className="absolute bottom-0 w-97 h-194 
+                    p-7 flex flex-col justify-center items-center 
+                    gap-2.5 z-20 bg-[#DDE9F6]"> */}
+            <div className="absolute bottom-0 w-97 h-194 
+                    p-7 flex flex-col justify-center items-center 
+                    gap-2.5 z-20 bg-[#DDE9F6]">
+                        
+                <div className="absolute top-5 right-5"
+                    onClick={openChatBotPage}>
+                    <img src="/public/icons/X-icon.svg" />
                 </div>
-            ) : null
-        }
-        </>
+                
+                <div>
+                    <div>
+                        날짜 및 분리선
+                    </div>
+                </div>
+
+
+                <div>
+                    <div>
+                        챗봇 예시 답안
+                    </div>
+                </div>
+
+
+                <div>
+                    <div>
+                        챗봇 예시 질문
+                    </div>
+                </div>
+
+                <div>
+                    <input placeholder="챗봇 예시 입력"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import LowerNav from "../components/LowerNav.tsx";
 import ChatBotButton from "../components/ChatBotButton.tsx";
+import ChatBotFrame from "../components/ChatBotFrame.tsx";
+import { useChatBotPageStore } from "../store/store.ts";
 
 const MainLayout = () => {
+    const {isChatBotOpen} = useChatBotPageStore();
     return (
         <>
             <main className="relative flex flex-col justify-between w-[390px] min-h-[844px] bg-[#f6f6f6]">
                 <Outlet /> {/* 캘린더, 시간표, 리뷰, 마이페이지 등 등이 여기에 렌더링됨 */}
                 <ChatBotButton />
+                {isChatBotOpen ? <ChatBotFrame/> : null}
                 <LowerNav />
             </main>
         </>
