cbf869a0604bec74ef32853fe3ac93cbdea74dac
glyph8 <newdy0808@gmail.com>
2025-06-02 19:06:45 +0900

    vercerl.json 추가

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,8 @@
-import {Outlet, useLocation} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 const AuthLayout = () =>{
-    const location = useLocation()
-
     return(
         <>
-            {location.pathname === '/' ? <div>루트 페이지</div> : null}
-
             <main className="w-[390px] min-h-[844px] bg-[#f6f6f6]">
                 <Outlet/> {/* LoginPage, SignupPage 등이 여기에 렌더링됨 */}
             </main>
