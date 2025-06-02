77f429d7310e65857a58d04e0a79262042206922
glyph8 <newdy0808@gmail.com>
2025-06-02 15:56:40 +0900

    base 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Capstone2025-Frontend',
   plugins: [react(), tailwindcss(),],
 })
